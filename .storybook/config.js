48e3d913bcfdd4841ceacc361fe5ce657b521de6
Ryan Seddon <seddon.ryan@gmail.com>
2019-01-02 16:11:42 +1100

    chore(ci): Add travis dot file and build storybook command

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,26 @@
-import { configure } from '@storybook/react';
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import { configure, addDecorator } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../packages', true, /.stories.js$/);
+const req = require.context('../packages', true, /.stories.js$/u);
+
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(
+  withOptions({
+    name: 'React Containers',
+    addonPanelInRight: true,
+    url: 'https://github.com/zendeskgarden/react-containers'
+  })
+);
 
 configure(loadStories, module);
