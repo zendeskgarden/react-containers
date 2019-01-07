b232484e91b1c97ecf959b2f69b3307a320b4c20
Ryan Seddon <seddon.ryan@gmail.com>
2019-01-07 11:03:37 +1100

    Austin feedback

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,6 +6,9 @@
  */
 
 module.exports = (baseConfig, env, defaultConfig) => {
+  // In order for hooks to work with storybook we need to force a compatible
+  // version of hot-loader react-dom.
+  // See https://github.com/storybooks/storybook/issues/4691#issuecomment-447570189
   defaultConfig.resolve.alias['react-dom'] = '@hot-loader/react-dom';
   defaultConfig.module.rules.push({
     test: /stories.js$/u,
