ab9bce33cbae15f2375a51e607a60cf60b3de9e8
Ryan Seddon <seddon.ryan@gmail.com>
2019-01-23 17:07:38 +1100

    fix(build): run lerna bootstrap to link all deps correctly

--- packages/.template/stories.js
+++ packages/.template/stories.js
@@ -8,8 +8,6 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import { withKnobs } from '@storybook/addon-knobs';
 
 import { Button } from '@storybook/react/demo';
