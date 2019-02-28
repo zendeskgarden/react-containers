dc2cf6445e567e777f0dbe273ca38ceef3ca7fbc
Ryan Seddon <seddon.ryan@gmail.com>
2019-02-28 16:49:05 +1100

    feat(keyboardfocus): introduce useKeyboardFocus hook and KeyboardFocusContainer

---
+++ packages/keyboardfocus/README.md
@@ -0,0 +1,26 @@
+# @zendeskgarden/container-keyboardfocus [![npm version](https://img.shields.io/npm/v/@zendeskgarden/container-keyboardfocus.svg?style=flat-square)](https://www.npmjs.com/package/@zendeskgarden/container-keyboardfocus)
+
+This package includes containers relating to keyboardfocus in the
+[Garden Design System](https://zendeskgarden.github.io/).
+
+## Installation
+
+```sh
+npm install @zendeskgarden/container-keyboardfocus
+```
+
+## Usage
+
+```jsx static
+import { ExampleContainer } from '@zendeskgarden/container-keyboardfocus';
+
+<ExampleContainer>{({ getExampleProps }) => <div {...getExampleProps()} />}</ExampleContainer>;
+```
+
+<!--
+  TODO:
+
+  * [ ] Add keyboardfocus to root README table.
+  * [ ] Add keyboardfocus stories.js.
+  * [ ] Delete this comment block.
+-->
---
+++ packages/keyboardfocus/src/useKeyboardFocus.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import { useState } from 'react';
+
+import { composeEventHandlers } from '@zendeskgarden/container-selection';
+
+export function useKeyboardFocus() {
+  const [keyboardFocused, setKeyboardFocused] = useState(false);
+  let keyboardFocusable = true;
+
+  const onMouseDown = () => {
+    keyboardFocusable = false;
+
+    /**
+     * This is necessary to recognize focus events caused by keyboard vs mouseDown.
+     * Due to React event ordering this is always called before onFocus.
+     */
+    setTimeout(() => {
+      keyboardFocusable = true;
+    }, 0);
+  };
+
+  const onFocus = () => {
+    if (keyboardFocusable) {
+      setKeyboardFocused(true);
+    }
+  };
+
+  const onBlur = () => {
+    setKeyboardFocused(false);
+  };
+
+  const getFocusProps = ({ tabIndex = 0, ...props } = {}) => {
+    return {
+      tabIndex,
+      onBlur: composeEventHandlers(props.onBlur, onBlur),
+      onFocus: composeEventHandlers(props.onFocus, onFocus),
+      onMouseDown: composeEventHandlers(props.onMouseDown, onMouseDown),
+      ...props
+    };
+  };
+
+  return {
+    getFocusProps,
+    keyboardFocused
+  };
+}
