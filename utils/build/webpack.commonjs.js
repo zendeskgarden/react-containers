9812fb319a6072dcd0a55f453f9ae2fa003edaa0
Ryan Seddon <seddon.ryan@gmail.com>
2019-03-06 13:59:30 +1100

    chore(build): Remove UMD builds (#13)

--- utils/build/webpack.commonjs.js
+++ utils/build/webpack.commonjs.js
@@ -6,19 +6,60 @@
  */
 
 const path = require('path');
-const merge = require('webpack-merge');
 const nodeExternals = require('webpack-node-externals');
-const baseConfig = require('./webpack.base');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const packageManifest = require(path.resolve('package.json'));
+const babelOptions = require(path.resolve('../../babel.config.js'));
 
-module.exports = merge(baseConfig, {
+const options = {
+  mode: 'production',
+  entry: path.resolve('src', 'index.js'),
   output: {
     path: path.resolve('dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs'
+  },
+  devtool: 'source-map',
+  plugins: [
+    /**
+     * Allows the `data-garden-version` attribute to be applied to all
+     * views without importing entire package.json into the bundle
+     */
+    new webpack.DefinePlugin({
+      PACKAGE_VERSION: JSON.stringify(packageManifest.version)
+    }),
+    new webpack.BannerPlugin(
+      `
+Copyright Zendesk, Inc.
+Use of this source code is governed under the Apache License, Version 2.0
+found at http://www.apache.org/licenses/LICENSE-2.0
+    `.trim()
+    )
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/u,
+        exclude: /node_modules/u,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: babelOptions
+          }
+        ]
+      }
+    ]
   },
   externals: [
     nodeExternals({
       modulesFromFile: true
     })
   ]
-});
+};
+
+if (process.env.ANALYZE_BUNDLE) {
+  options.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'static' }));
+}
+
+module.exports = options;
