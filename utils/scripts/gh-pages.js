f64749fbf5193b0627c63aa2e477213466f0b3d0
Jonathan Zempel <jzempel@gmail.com>
2020-03-05 16:46:45 -0800

    Add initial circle config with gh-pages deploy

---
+++ utils/scripts/gh-pages.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+const ghPages = require('gh-pages');
+
+ghPages.publish('demo', {
+  repo: `https://${process.env.GITHUB_TOKEN}@github.com/zendeskgarden/react-containers.git`,
+  silent: true
+});
