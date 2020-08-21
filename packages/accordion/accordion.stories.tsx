4bd46a7332e89c4e7d59d8e9e9dcfbbf874d9821
hzhu <hz@henryzhu.me>
2020-08-21 12:19:09 -0700

    Update to Garden visually hidden

--- packages/accordion/accordion.stories.tsx
+++ packages/accordion/accordion.stories.tsx
@@ -12,14 +12,13 @@ import { AccordionContainer, useAccordion, IUseAccordionReturnValue } from './sr
 
 const visuallyHidden: CSSProperties = {
   position: 'absolute',
+  border: '0',
+  clip: 'rect(1px, 1px, 1px, 1px)',
+  padding: '0',
   width: '1px',
   height: '1px',
-  padding: '0',
-  margin: '-1px',
   overflow: 'hidden',
-  clip: 'rect(0, 0, 0, 0)',
-  whiteSpace: 'nowrap',
-  borderWidth: '0'
+  whiteSpace: 'nowrap'
 };
 
 export const Container = () => {
