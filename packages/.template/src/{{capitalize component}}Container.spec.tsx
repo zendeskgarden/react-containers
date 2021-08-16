f7e1d748e667ac3b7a5829c612db33a39971a6cd
Jonathan Zempel <jzempel@gmail.com>
2021-08-16 13:32:42 -0400

    chore: enhance `yarn new` script (#342)

---
+++ packages/.template/src/{{capitalize component}}Container.spec.tsx
@@ -0,0 +1,30 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { {{capitalize component}}Container } from './';
+import { IUse{{capitalize component}}ReturnValue } from './use{{capitalize component}}';
+
+describe('{{capitalize component}}Container', () => {
+  const Test{{capitalize component}} = () => (
+    <{{capitalize component}}Container>
+      {({ get{{capitalize component}}Props }: IUse{{capitalize component}}ReturnValue) => (
+        <div {...get{{capitalize component}}Props({ 'data-test-id': 'div' })} />
+      )}
+    </{{capitalize component}}Container>
+  );
+
+  describe('get{{capitalize component}}Props', () => {
+    it('applies correct accessibility role', () => {
+      const { getByTestId } = render(<Test{{capitalize component}} />);
+      const element = getByTestId('div');
+
+      expect(element).toHaveAttribute('role', 'region');
+    });
+  });
+});
