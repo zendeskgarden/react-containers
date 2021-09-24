e2e5ef80da044acb0f3a979c37e977a58927b85e
Mike Tobia <Francois-Esquire@users.noreply.github.com>
2021-09-24 15:11:54 -0400

    chore(utilities): deprecate use-combined-refs hook (#382)

--- packages/utilities/src/utils/useCombinedRefs.ts
+++ packages/utilities/src/utils/useCombinedRefs.ts
@@ -12,11 +12,24 @@ type PossibleRef<T> = ((instance: T | null) => void) | RefObject<T> | null | und
 /**
  * A custom hook that combines multiple refs into a single, valid ref.
  * @param  {...any} refs
+ *
+ * @deprecated Will be removed in v1. Please use `react-merge-refs` instead.
+ *
  */
 export function useCombinedRefs<T>(...refs: PossibleRef<T>[]) {
   const targetRef = useRef<T>(null);
 
   useEffect(() => {
+    /**
+     * Deprecation warning for consumers:
+     * placing the warning in `useEffect` hook scope so it
+     * is emitted only on each mount of the hook, when used.
+     */
+    // eslint-disable-next-line no-console
+    console.warn(
+      '"useCombinedRefs()" has been deprecated and will be removed in the next major version release of "@zendeskgarden/container-utilities"'
+    );
+
     refs.forEach(ref => {
       if (!ref) return;
 
