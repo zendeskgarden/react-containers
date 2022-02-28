c0475f36d0eacf61736be564b27c29a0cc5568f0
Jonathan Zempel <jzempel@gmail.com>
2022-02-28 17:57:32 -0500

    Add selection with focus visible pattern

--- packages/focusvisible/demo/stories/FocusVisibleStory.tsx
+++ packages/focusvisible/demo/stories/FocusVisibleStory.tsx
@@ -7,6 +7,7 @@
 
 import React, { forwardRef, RefObject } from 'react';
 import { Story } from '@storybook/react';
+import { createGlobalStyle } from 'styled-components';
 import { PALETTE } from '@zendeskgarden/react-theming';
 import {
   FocusVisibleContainer,
@@ -14,66 +15,60 @@ import {
   IUseFocusVisibleProps,
   useFocusVisible
 } from '@zendeskgarden/container-focusvisible';
-import { createGlobalStyle } from 'styled-components';
 
-interface IComponentProps {
-  className?: IUseFocusVisibleProps['className'];
-  dataAttribute?: IUseFocusVisibleProps['dataAttribute'];
-}
-
-const Component = forwardRef<HTMLDivElement, IComponentProps>(
-  ({ className, dataAttribute }, ref) => {
-    const GlobalStyle = createGlobalStyle`
-    :focus {
-      outline: none;
-    }
-
-    .${className},
-    [${dataAttribute}='true'] {
-      box-shadow: 0 0 0 2px ${PALETTE.green[400]};
-    }
-
-    blockquote p::before {
-      content: '“';
-    }
-
-    blockquote p::after {
-      content: '”';
-    }
-  `;
-
-    /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-    return (
-      <>
-        <GlobalStyle />
-        <div ref={ref}>
-          <a href="#test">Anchor</a>
-          <br />
-          <button className="my-5 px-2 py-1" type="button">
-            Button
-          </button>
-          <br />
-          <label>
-            <span>Input</span>
-            <input className="ml-1" />
-          </label>
-          <br />
-          <blockquote className="my-5" contentEditable="true">
-            <p>Content editable block quote</p>
-          </blockquote>
-          <div tabIndex={0}>Custom [tabindex=&quot;0&quot;]</div>
-        </div>
-      </>
-    );
+const GlobalStyle = createGlobalStyle`
+  :focus {
+    outline: none;
   }
-);
+
+  .garden-focus-visible,
+  [data-garden-focus-visible='true'] {
+    box-shadow: 0 0 0 2px ${PALETTE.green[400]};
+  }
+
+  blockquote p::before {
+    content: '“';
+  }
+
+  blockquote p::after {
+    content: '”';
+  }
+`;
+
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+const Component = forwardRef<HTMLDivElement, any>((_, ref) => (
+  <>
+    <GlobalStyle />
+    <div ref={ref}>
+      <a href="#test">Anchor</a>
+      <br />
+      <button className="my-5 px-2 py-1" type="button">
+        Button
+      </button>
+      <br />
+      <label>
+        <span>Input</span>
+        <input className="ml-1" />
+      </label>
+      <div className="my-5" tabIndex={0}>
+        Custom [tabindex=&quot;0&quot;]
+      </div>
+      <label>
+        <span className="align-top">Textarea</span>
+        <textarea className="ml-1" />
+      </label>
+      <br />
+      <blockquote className="my-5" contentEditable suppressContentEditableWarning>
+        <p>Content editable block quote</p>
+      </blockquote>
+    </div>
+  </>
+));
 
 Component.displayName = 'Component';
 
-const Container = ({ className, dataAttribute }: IFocusVisibleContainerProps) => (
-  <FocusVisibleContainer className={className} dataAttribute={dataAttribute}>
-    {({ ref }) => <Component className={className} dataAttribute={dataAttribute} ref={ref} />}
-  </FocusVisibleContainer>
+const Container = () => (
+  <FocusVisibleContainer>{({ ref }) => <Component ref={ref} />}</FocusVisibleContainer>
 );
 
 const Hook = ({ scope, ...props }: IUseFocusVisibleProps) => {
@@ -93,7 +88,7 @@ export const FocusVisibleStory: Story<IArgs> = ({ as, ...props }) => {
   const FocusVisible = () => {
     switch (as) {
       case 'container':
-        return <Container {...props} />;
+        return <Container />;
 
       case 'hook':
       default:
