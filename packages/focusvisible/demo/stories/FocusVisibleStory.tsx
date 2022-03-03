d82a9ef996be340e205ed6b9af80fc134eb92bf7
Jonathan Zempel <jzempel@gmail.com>
2022-03-03 12:46:28 -0500

    chore(focusvisible): refactor storybook (#426)

---
+++ packages/focusvisible/demo/stories/FocusVisibleStory.tsx
@@ -0,0 +1,100 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React, { forwardRef, RefObject } from 'react';
+import { Story } from '@storybook/react';
+import { createGlobalStyle } from 'styled-components';
+import { PALETTE } from '@zendeskgarden/react-theming';
+import {
+  FocusVisibleContainer,
+  IFocusVisibleContainerProps,
+  IUseFocusVisibleProps,
+  useFocusVisible
+} from '@zendeskgarden/container-focusvisible';
+
+const GlobalStyle = createGlobalStyle`
+  :focus {
+    outline: none;
+  }
+
+  .garden-focus-visible,
+  [data-garden-focus-visible='true'] {
+    box-shadow: 0 0 0 2px ${PALETTE.green[400]};
+  }
+
+  blockquote p::before {
+    content: '“';
+  }
+
+  blockquote p::after {
+    content: '”';
+  }
+`;
+
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+const Component = forwardRef<HTMLDivElement, any>((_, ref) => (
+  <>
+    <GlobalStyle />
+    <div ref={ref}>
+      <a href="#test">Anchor</a>
+      <br />
+      <button className="my-5 px-2 py-1" type="button">
+        Button
+      </button>
+      <br />
+      <label>
+        <span>Input</span>
+        <input className="ml-1" />
+      </label>
+      <div className="my-5" tabIndex={0}>
+        Custom [tabindex=&quot;0&quot;]
+      </div>
+      <label>
+        <span className="align-top">Textarea</span>
+        <textarea className="ml-1" />
+      </label>
+      <br />
+      <blockquote className="my-5" contentEditable suppressContentEditableWarning>
+        <p>Content editable block quote</p>
+      </blockquote>
+    </div>
+  </>
+));
+
+Component.displayName = 'Component';
+
+const Container = () => (
+  <FocusVisibleContainer>{({ ref }) => <Component ref={ref} />}</FocusVisibleContainer>
+);
+
+const Hook = ({ scope, ...props }: IUseFocusVisibleProps) => {
+  useFocusVisible({ scope, ...props });
+
+  const ref = scope as RefObject<HTMLDivElement>;
+
+  return <Component {...props} ref={ref} />;
+};
+
+interface IArgs extends IFocusVisibleContainerProps {
+  as: 'hook' | 'container';
+  scope: RefObject<HTMLElement>;
+}
+
+export const FocusVisibleStory: Story<IArgs> = ({ as, ...props }) => {
+  const FocusVisible = () => {
+    switch (as) {
+      case 'container':
+        return <Container />;
+
+      case 'hook':
+      default:
+        return <Hook {...props} />;
+    }
+  };
+
+  return <FocusVisible />;
+};
