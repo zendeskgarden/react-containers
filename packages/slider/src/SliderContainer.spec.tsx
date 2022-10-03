393457b88da6d6cdcfd860d0d09c0a43d72b7169
Jen Gorfine <jen.gorfine@zendesk.com>
2022-10-03 16:41:46 -0400

    feat(slider): wrote tests for Slider prop getters

--- packages/slider/src/SliderContainer.spec.tsx
+++ packages/slider/src/SliderContainer.spec.tsx
@@ -7,19 +7,26 @@
 
 import React, { createRef } from 'react';
 import { render } from '@testing-library/react';
-import { IUseSliderReturnValue } from './types';
+import { IUseSliderProps, IUseSliderReturnValue } from './types';
 import { SliderContainer } from './SliderContainer';
 
 describe('SliderContainer', () => {
-  const TestSlider = () => {
+  const TestSlider = ({ min = 0, max = 100, ...props }: Partial<IUseSliderProps>) => {
     const trackRef = createRef<HTMLDivElement>();
     const minThumbRef = createRef<HTMLDivElement>();
     const maxThumbRef = createRef<HTMLDivElement>();
 
     return (
-      <SliderContainer trackRef={trackRef} minThumbRef={minThumbRef} maxThumbRef={maxThumbRef}>
+      <SliderContainer
+        min={min}
+        max={max}
+        trackRef={trackRef}
+        minThumbRef={minThumbRef}
+        maxThumbRef={maxThumbRef}
+        {...props}
+      >
         {({ getTrackProps, getMinThumbProps, getMaxThumbProps }: IUseSliderReturnValue) => (
-          <div {...getTrackProps()}>
+          <div data-test-id="track" {...getTrackProps()}>
             <div data-test-id="min_thumb" {...getMinThumbProps({ 'aria-label': 'min' })} />
             <div data-test-id="max_thumb" {...getMaxThumbProps({ 'aria-label': 'max' })} />
           </div>
@@ -28,12 +35,102 @@ describe('SliderContainer', () => {
     );
   };
 
-  describe('getThumbProps', () => {
-    it('applies correct accessibility role', () => {
-      const { getByTestId } = render(<TestSlider />);
-      const element = getByTestId('min_thumb');
+  describe('prop getters', () => {
+    describe('getTrackProps', () => {
+      it('renders with correct default attributes', () => {
+        const { getByTestId } = render(<TestSlider />);
+        const element = getByTestId('track');
 
-      expect(element).toHaveAttribute('role', 'slider');
+        expect(element).toHaveAttribute('data-garden-container-id', 'containers.slider.track');
+        expect(element).toHaveAttribute('data-garden-container-version');
+      });
+
+      it('renders with the aria-disabled attribute, when disabled', () => {
+        const { getByTestId } = render(<TestSlider disabled />);
+        const element = getByTestId('track');
+
+        expect(element).toHaveAttribute('aria-disabled', 'true');
+      });
+    });
+
+    describe('getMinThumbProps', () => {
+      it('renders with correct default attributes', () => {
+        const { getByTestId } = render(<TestSlider />);
+        const element = getByTestId('min_thumb');
+
+        expect(element).toHaveAttribute('data-garden-container-id', 'containers.slider.thumb');
+        expect(element).toHaveAttribute('data-garden-container-version');
+        expect(element).toHaveAttribute('tabindex', '0');
+        expect(element).toHaveAttribute('role', 'slider');
+        expect(element).toHaveAttribute('aria-valuenow', '0');
+        expect(element).toHaveAttribute('aria-valuemin', '0');
+        expect(element).toHaveAttribute('aria-valuemax', '100');
+      });
+
+      it('renders with a -1 tabindex, when disabled', () => {
+        const { getByTestId } = render(<TestSlider disabled />);
+        const element = getByTestId('min_thumb');
+
+        expect(element).toHaveAttribute('tabindex', '-1');
+      });
+
+      describe('uncontrolled', () => {
+        it('renders with default (starting) value', () => {
+          const { getByTestId } = render(<TestSlider defaultMinValue={25} />);
+          const element = getByTestId('min_thumb');
+
+          expect(element).toHaveAttribute('aria-valuenow', '25');
+        });
+      });
+
+      describe('controlled', () => {
+        it('accepts a passed in value', () => {
+          const { getByTestId } = render(<TestSlider minValue={25} />);
+          const element = getByTestId('min_thumb');
+
+          expect(element).toHaveAttribute('aria-valuenow', '25');
+        });
+      });
+    });
+
+    describe('getMaxThumbProps', () => {
+      it('renders with correct default attributes', () => {
+        const { getByTestId } = render(<TestSlider />);
+        const element = getByTestId('max_thumb');
+
+        expect(element).toHaveAttribute('data-garden-container-id', 'containers.slider.thumb');
+        expect(element).toHaveAttribute('data-garden-container-version');
+        expect(element).toHaveAttribute('tabindex', '0');
+        expect(element).toHaveAttribute('role', 'slider');
+        expect(element).toHaveAttribute('aria-valuenow', '100');
+        expect(element).toHaveAttribute('aria-valuemin', '0');
+        expect(element).toHaveAttribute('aria-valuemax', '100');
+      });
+
+      it('renders with a -1 tabindex, when disabled', () => {
+        const { getByTestId } = render(<TestSlider disabled />);
+        const element = getByTestId('max_thumb');
+
+        expect(element).toHaveAttribute('tabindex', '-1');
+      });
+
+      describe('uncontrolled', () => {
+        it('renders with default (starting) value', () => {
+          const { getByTestId } = render(<TestSlider defaultMaxValue={50} />);
+          const element = getByTestId('max_thumb');
+
+          expect(element).toHaveAttribute('aria-valuenow', '50');
+        });
+      });
+
+      describe('controlled', () => {
+        it('accepts a passed in value', () => {
+          const { getByTestId } = render(<TestSlider maxValue={50} />);
+          const element = getByTestId('max_thumb');
+
+          expect(element).toHaveAttribute('aria-valuenow', '50');
+        });
+      });
     });
   });
 });
